7d97dbde41722a66dcfb366ac51dd18161737b48
Maschmo <martin.schmoeller91@gmail.com>
2021-06-06 20:54:32 +0200

    asdf

--- client/src/Context.js
+++ client/src/Context.js
@@ -4,7 +4,6 @@ import Peer from 'simple-peer';
 
 const SocketContext = createContext();
 
-//const socket = io('http://localhost:5000');
 const socket = io('https://bitiwebrtc.herokuapp.com');
 
 const ContextProvider = ({ children }) => {
